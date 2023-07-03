30233263eb7416119cab870944fe4120e00b9136
Tactless7 <blier.juliane@gmail.com>
2023-07-03 21:11:47 +0200

    chore(frontend): setpup front base

--- front/src/stores/counter.ts
+++ front/src/stores/counter.ts
@@ -1,12 +1,12 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import {ref} from 'vue'
+import {defineStore} from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useOrderStore = defineStore('orders', () => {
+  const orders = ref([])
+
+  const fetchOrders = () => {
+    orders.value = []
   }
 
-  return { count, doubleCount, increment }
+  return {orders, fetchOrders}
 })
---
+++ front/src/views/listing/ListingPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="listing-page">
+    <h1>Orders List</h1>
+
+
+  </div>
+</template>
+
+<style scoped>
+</style>
